121f40ad14f5c20bb38aee79d0a829ebef24f3db
Frank Wagner <frank.wagner.13@gmail.com>
2012-05-02 19:31:26 +0200

    use resolved coverage.jar dependency; test clike for text style

--- test/modestest.js
+++ test/modestest.js
@@ -5,6 +5,7 @@ ModesTest.prototype.loadModeIndexHtml = function(mode, queue) {
 	var frame = null;
 	queue.call('load ' + path, function(callbacks) {
 		var doc = window.document;
+
 		frame = doc.createElement('iframe');
 		frame.id = mode;
 		var handler = callbacks.add(function() {
@@ -12,12 +13,29 @@ ModesTest.prototype.loadModeIndexHtml = function(mode, queue) {
 		});
 		frame.src = path;
 		frame.onload = function() {
-			handler();
+			if (this.readyState == undefined || this.readyState == 'complete'
+					|| this.readyState == 'loaded') {
+				frame.onload = null;
+				handler();
+			}
 		};
 		doc.body.appendChild(frame);
+		if (jstestdriver.global.LCOV) {
+			frame.contentWindow.LCOV = jstestdriver.global.LCOV;
+		}
 	});
 
-	queue.call('discover CodeMirror ', function(callbacks) {
+	/*
+	 * queue.call('wait CodeMirror', function(callbacks) { // wait some more var
+	 * t = null; var done = callbacks.add(function() { if (t) {
+	 * window.clearInterval(t); t = null; } }); var doc =
+	 * frame.contentWindow.document; assertNotNull(doc); var that = this; t =
+	 * window.setInterval(function() { var codeMirror =
+	 * that.getElementsByClassName(doc, 'div', 'CodeMirror')[0]; if (codeMirror) {
+	 * done(); } }, 500); });
+	 */
+
+	queue.call('discover CodeMirror', function(callbacks) {
 		assertNotNull(frame);
 		var doc = frame.contentWindow.document;
 		assertNotNull(doc);
@@ -58,9 +76,32 @@ ModesTest.prototype.getElementsByClassName = function(element, tag, className) {
 	return result;
 };
 
+ModesTest.prototype.getElementsByText = function(element, tag, text) {
+	var result = [];
+	var candidates = element.getElementsByTagName(tag);
+	for ( var i = 0; i < candidates.length; i++) {
+		if (text === candidates[i].textContent) {
+			result.push(candidates[i]);
+		}
+	}
+	return result;
+};
+
 ModesTest.prototype.countCodeMirrorLines = function() {
 	var count = this._lines.getElementsByTagName('pre').length;
 	return count - 1;
+};
+
+ModesTest.prototype.assertCodeMirrorTextHasClass = function(text, className) {
+	var match = new RegExp("(^|\\s)" + className + "(\\s|$)");
+	var elements = this.getElementsByText(this._lines, 'span', text);
+	for (e in elements) {
+		// TODO find coord for e
+		assertTrue('unexpected "' + elements[e].className + '" for "'
+				+ elements[e].textContent + '"', match
+				.test(elements[e].className));
+	}
+	return elements.length;
 };
 
 ModesTest.prototype.test_clike = function(queue) {
@@ -70,6 +111,22 @@ ModesTest.prototype.test_clike = function(queue) {
 	queue.call('assert', function(callbacks) {
 		var lines = this.countCodeMirrorLines();
 		assertEquals(66, lines);
+
+		var count;
+
+		count = this.assertCodeMirrorTextHasClass('/* C demo code */',
+				'cm-comment');
+		assertEquals(1, count);
+
+		count = this.assertCodeMirrorTextHasClass('void', 'cm-keyword');
+		assertEquals(8, count);
+
+		count = this.assertCodeMirrorTextHasClass('pthread_create', 'cm-word');
+		assertEquals(1, count);
+
+		count = this.assertCodeMirrorTextHasClass('*', 'cm-operator');
+		assertEquals(13, count);
+
 		callbacks.noop()();
 	});
 };
