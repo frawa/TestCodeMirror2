297aa754b26538dfb5a9c18153b8d07f4e459b79
Frank Wagner <frank.wagner.13@gmail.com>
2012-05-04 21:25:36 +0200

    fix smalltalk mode for code coverage

--- mode/smalltalk/smalltalk.js
+++ mode/smalltalk/smalltalk.js
@@ -1,6 +1,6 @@
 CodeMirror.defineMode('smalltalk', function(config, modeConfig) {
 
-	var specialChars = /[+\-/\\*~<>=@%|&?!.:;^]/;
+	var specialChars = /[+\-\/\\*~<>=@%|&?!.:;^]/;
 	var keywords = /true|false|nil|self|super|thisContext/;
 
 	var Context = function(tokenizer, parent) {
