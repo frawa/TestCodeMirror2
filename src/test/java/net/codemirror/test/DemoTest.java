bc08147d6aec5a827ee7ea8592e683cb1b93b56e
Frank Wagner <frank.wagner.13@gmail.com>
2012-05-19 18:27:05 +0200

    test activeline demo

--- src/test/java/net/codemirror/test/DemoTest.java
+++ src/test/java/net/codemirror/test/DemoTest.java
@@ -21,7 +21,7 @@ import org.openqa.selenium.interactions.Actions;
 @RunWith(Parameterized.class)
 public class DemoTest {
 
-	//@Parameters(testDescription="{0}[{2}]")
+	// @Parameters(testDescription="{0}[{2}]")
 	@Parameters
 	public static List<Object[]> browsers() {
 		return TestHelper.getBrowsers();
@@ -29,29 +29,53 @@ public class DemoTest {
 
 	private WebDriver driver;
 
-	public DemoTest(String browser) {		
+	public DemoTest(String browser) {
 		driver = TestHelper.getDriver(browser);
 	}
-	
+
 	@AfterClass
 	public static void afterClass() {
 		TestHelper.close();
 	}
 
-	private WebElement findWord(int row, String colText, int occurence) {
+	private int getLinesToSkip(List<WebElement> lines) {
+		// skip to zero line pre, of class CodeMirror-cursor
+		int skip = 0;
+		for (; skip < lines.size(); skip++) {
+			if ("CodeMirror-cursor".equals(lines.get(skip)
+					.getAttribute("class"))) {
+				break;
+			}
+		}
+		return skip;
+	}
+
+	private WebElement getTextLine(List<WebElement> lines, int lineno) {
+		int skip = getLinesToSkip(lines);
+		for (int i=skip+lineno; i<lines.size(); i++) {
+			WebElement line = lines.get(i);
+			// pre's for meta purposes have classes set
+			if ( line.getAttribute("class").length()==0 ) {
+				return line;
+			}
+		}
+		Assert.fail("no test line #"+lineno);
+		return null;
+	}
+	
+	private WebElement findWord(int lineno, String colText, int occurence) {
 		WebElement linesContainer = driver.findElements(
 				By.className("CodeMirror-lines")).get(0);
 		Assert.assertNotNull(linesContainer);
 
 		List<WebElement> lines = linesContainer.findElements(By.tagName("pre"));
-		// row is zero-based, lines.get(0) is a helper pre
-		WebElement line = lines.get(row);
+		WebElement line = getTextLine(lines,lineno);
 
 		List<WebElement> found = line.findElements(By.xpath("*[text()=\""
 				+ colText + "\"]"));
-		Assert.assertTrue("row #" + row + " does not contain " + colText,
+		Assert.assertTrue("row #" + lineno + " does not contain " + colText,
 				found.size() > 0);
-		Assert.assertTrue("row #" + row + " does not contain " + occurence
+		Assert.assertTrue("row #" + lineno + " does not contain " + occurence
 				+ " times " + colText, found.size() > occurence);
 
 		WebElement result = found.get(occurence);
@@ -59,14 +83,14 @@ public class DemoTest {
 
 		return result;
 	}
-	
+
 	@Test
 	public void complete() {
 		Assume.assumeTrue(TestHelper.hasNativeEvents(driver));
-		
+
 		URL url = TestHelper.getURL("demo/complete.html");
-		driver.get(url.toString());		
-		
+		driver.get(url.toString());
+
 		WebElement v = findWord(30, "v", 0);
 
 		Actions actions = new Actions(driver);
@@ -87,12 +111,75 @@ public class DemoTest {
 		WebElement v = findWord(30, "v", 0);
 
 		Actions actions = new Actions(driver);
-		int w = v.getSize().width;
 		actions.click(v).sendKeys(Keys.ARROW_RIGHT)
 				.sendKeys(Keys.chord(Keys.CONTROL, Keys.SPACE)).perform();
 
 		List<WebElement> completions = driver.findElements(By
 				.className("CodeMirror-completions"));
 		Assert.assertTrue("completions missing", completions.size() == 1);
+	}
+
+	private WebElement assertClass(String className, int lineno) {
+		WebElement linesContainer = driver.findElements(
+				By.className("CodeMirror-lines")).get(0);
+		Assert.assertNotNull(linesContainer);
+
+		List<WebElement> lines = linesContainer.findElements(By.tagName("pre"));
+
+		WebElement line = lines.get(getLinesToSkip(lines) + lineno);
+		Assert.assertNotNull("no line #" + lineno, line);
+		Assert.assertEquals(className, line.getAttribute("class"));
+
+		return line;
+	}
+
+	private void assertNotClass(String className, int lineno) {
+		WebElement linesContainer = driver.findElements(
+				By.className("CodeMirror-lines")).get(0);
+		Assert.assertNotNull(linesContainer);
+
+		List<WebElement> lines = linesContainer.findElements(By.tagName("pre"));
+		WebElement line = lines.get(getLinesToSkip(lines) + lineno);
+		Assert.assertNotNull("no line #" + lineno, line);
+		Assert.assertTrue("unexpected class " + className,
+				!className.equals(line.getAttribute("class")));
+	}
+
+	private Actions sendKeys(Actions actions, Keys keys, int count) {
+		for (int i = 0; i < count; i++) {
+			actions = actions.sendKeys(keys);
+		}
+		return actions;
+	}
+
+	@Test
+	public void activeline() {
+		URL url = TestHelper.getURL("demo/activeline.html");
+		driver.get(url.toString());
+
+		assertClass("activeline", 1);
+
+		WebElement c = findWord(5, "<channel>", 0);
+		c.click();
+		assertNotClass("activeline", 1);
+		assertClass("activeline", 5);
+
+		c = findWord(5, "<channel>", 0);
+		Actions actions = new Actions(driver);
+		actions.click(c).sendKeys(Keys.ARROW_DOWN).build().perform();
+		assertNotClass("activeline", 5);
+		assertClass("activeline", 6);
+		
+		c = findWord(5, "<channel>", 0);
+		Actions actions2 = new Actions(driver);
+		actions2.click(c).sendKeys(Keys.DOWN).build().perform();
+		assertNotClass("activeline", 5);
+		assertClass("activeline", 6);
+
+		c = findWord(5, "<channel>", 0);
+		Actions actions3 = new Actions(driver);
+		sendKeys(actions3.click(c), Keys.ARROW_DOWN, 5).build().perform();
+		assertNotClass("activeline", 5);
+		assertClass("activeline", 10);
 	}
 }
